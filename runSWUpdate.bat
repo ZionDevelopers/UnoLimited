c3812d918fbc46528a7df0e6f777e96403531ffe
Júlio C. Oliveira <talk@juliocesar.me>
2023-06-03 19:50:32 -0300

    Update runSWUpdate.bat

--- runSWUpdate.bat
+++ runSWUpdate.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d G:\SteamLibrary\SteamApps\common\GarrysMod\bin
+cd /d V:\SteamLibrary\SteamApps\common\GarrysMod\bin
 gmad.exe create -folder "D:/Github/UnoLimited" -out "D:/Github/UnoLimited.gma"
 gmpublish.exe update -addon "D:/Github/UnoLimited.gma" -id 187192556
 pause
